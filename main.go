68859e16dbb459567f6d23ea3838013aa91a66c5
Sebastien Brousse <prestasin43@ouest-france.fr>
2018-09-01 13:35:52 +0200

    Amélioration du dynamic invertory

--- main.go
+++ main.go
@@ -17,16 +17,27 @@ func main() {
 		panic("required environmental variables are not set")
 	}
 
-	dynamicInventory := make(map[string][]string)
+	if os.Args[1] == "--list" {
+		dynamicInventory := make(map[string][]string)
+		getServers(dynamicInventory, token, orgToken)
 
-	getServers(dynamicInventory, token, orgToken)
+		body, err := json.Marshal(dynamicInventory)
+		if err != nil {
+			panic("failed to marshal the dynamic inventory")
+		}
 
-	body, err := json.Marshal(dynamicInventory)
-	if err != nil {
-		panic("failed to marshal the dynamic inventory")
+		fmt.Println(string(body))
+	} else if os.Args[1] == "--host" {
+		dynamicInventory := make(map[string]string)
+		getServersDetails(dynamicInventory, token, orgToken, os.Args[2])
+
+		jsonString, err := json.Marshal(dynamicInventory)
+		if err != nil {
+			panic("failed to marshal the dynamic inventory")
+		}
+
+		fmt.Println(string(jsonString))
 	}
-
-	fmt.Println(string(body))
 }
 
 func getServers(dict map[string][]string, token, orgToken string) {
@@ -50,8 +61,46 @@ func getServers(dict map[string][]string, token, orgToken string) {
 			if _, ok := dict[tag]; !ok {
 				dict[tag] = make([]string, 0)
 			}
+			dict[tag] = append(dict[tag], server.Name)
+		}
+	}
+}
 
-			dict[tag] = append(dict[tag], server.PrivateIP)
+func getServersDetails(dict map[string]string, token, orgToken string, servername string) {
+	disabledLoggerFunc := func(a *api.ScalewayAPI) {
+		a.Logger = api.NewDisableLogger()
+	}
+
+	api, err := api.NewScalewayAPI(orgToken, token, "Scaleway Dynamic Inventory",
+		"", disabledLoggerFunc)
+	if err != nil {
+		panic(fmt.Sprintf("failed to create API instance: %s", err))
+	}
+
+	servers, err := api.GetServers(true, 0)
+	if err != nil {
+		panic(fmt.Sprintf("failed to get servers: %s", err))
+	}
+
+	var count int = 0
+	var publicip string = "none"
+	var user string = "root"
+	var bastionName string = "bastion"
+	for _, server := range *servers {
+		count++
+		if server.Name == bastionName {
+			publicip = server.PublicAddress.IP
+		}
+		if server.Name == servername {
+			if server.PublicAddress.IP != "" {
+				dict["ansible_host"] = server.PublicAddress.IP
+			} else {
+				dict["ansible_host"] = server.PrivateIP
+			}
+			dict["ansible_user"] = user
+		}
+		if !strings.Contains(servername, bastionName) {
+			dict["ansible_ssh_common_args"] = "-o ProxyCommand=\"ssh -W %h:%p -q " + user + "@" + publicip + "\""
 		}
 	}
 }
